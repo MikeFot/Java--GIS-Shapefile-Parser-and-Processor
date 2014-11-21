b21fe04d4a737fd8bc409b0bcc7b5c5dc9de572e
unknown <michaelfotiadis@gmail.com>
2014-11-21 15:17:22 +0000

    Added option of opening exported CSV files

--- src/com/michaelfotiadis/shpparser/export/csv/ExportCSV.java
+++ src/com/michaelfotiadis/shpparser/export/csv/ExportCSV.java
@@ -1,11 +1,16 @@
 package com.michaelfotiadis.shpparser.export.csv;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
 
 import com.michaelfotiadis.shpparser.containers.ergo.geometry.ErgoPolyline;
 import com.michaelfotiadis.shpparser.containers.ergo.geometry.ErgoReferenceSystem;
 import com.michaelfotiadis.shpparser.containers.file.ShapefileContainer;
 import com.michaelfotiadis.shpparser.util.file.FileOperations;
+import com.michaelfotiadis.shpparser.util.system.Log;
 
 /**
  * Class handling export to CSV
@@ -14,17 +19,16 @@ import com.michaelfotiadis.shpparser.util.file.FileOperations;
  */
 public class ExportCSV {
 
-	private final String CSV_EXTENSION = ".csv";
 	private final char DELIMITER = ',';
 
 	/**
 	 * Generates export contents and writes them to a user selected CSV
-	 * @param saveFile 
-	 * @param shapefileContainer
-	 * @param sourceCRS
-	 * @param targetCRS
+	 * @param saveFile File to be stored
+	 * @param shapefileContainer ShapefileContainer containing data to be exported
+	 * @param sourceCRS Source ErgoReferenceSystem
+	 * @param targetCRS Target ErgoReferenceSystem (null if no transformation)
 	 */
-	public void createAndExportCSV(File saveFile, final ShapefileContainer shapefileContainer,
+	public boolean createAndExportCSV(File saveFile, final ShapefileContainer shapefileContainer,
 			final ErgoReferenceSystem sourceCRS, final ErgoReferenceSystem targetCRS) {
 
 		boolean doTransformation = true;
@@ -53,8 +57,22 @@ public class ExportCSV {
 
 			new FileOperations().writeToFile(saveFile.getAbsolutePath(), builder.toString(), true, true);
 		} // end iteration
-
+		
+		int userChoice = JOptionPane.showConfirmDialog(
+				null,
+				"Attempt to Open Exported File?",
+				"Show Export",
+				JOptionPane.YES_NO_OPTION);
+		if (userChoice == JOptionPane.YES_OPTION) {
+			// Open the file using default options
+			try {
+				Desktop.getDesktop().open(saveFile);
+			} catch (IOException e) {
+				Log.Exception(e, 0);
+			} // remove if needed
+		}
+		
+		return true;
 	}
-
 
 }
