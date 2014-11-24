1152998b3398868bb7cb73277b880f1c32d1b70e
unknown <michaelfotiadis@gmail.com>
2014-11-24 14:34:37 +0000

    Fixed an issue with duplicate fields (ID, COORDINATES) in the ExportSQL class

--- src/com/michaelfotiadis/shpparser/export/sql/ExportSQL.java
+++ src/com/michaelfotiadis/shpparser/export/sql/ExportSQL.java
@@ -1,8 +1,8 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package com.michaelfotiadis.shpparser.export.sql;
 
@@ -33,181 +33,203 @@ import com.michaelfotiadis.shpparser.util.system.Log;
  * @author Michael Fotiadis
  */
 public class ExportSQL {
-    
-    public void insertDataToTable(int ID, ErgoPolyline line, String schema) {
-        PreparedStatement prepStmt = null;
-        
-        try {
-            // use a prepared statement to avoid injections
-            
-            Log.Out("Processing " + ID + " for object " + line, 2, false);
-            
-            
-            prepStmt = Singleton.getConnection().prepareStatement(schema);
-            
-            int countColumn = 1;
-            
-            // set ? variables by index order
-            prepStmt.setInt(countColumn, ID);
-            
-            // add coordinates
-            countColumn ++;
-            prepStmt.setString(countColumn, line.getVertexListAsString());
-            
-            for (String key : line.getStringKeys()) {
-                countColumn++;
-                prepStmt.setString(countColumn, String.valueOf(line.getString(key)));
-                
-            }
-            
-            // get 0 or 1, depending on success
-            int result = prepStmt.executeUpdate();
-            
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            Log.Exception(e, 1);
-            
-        } finally {
-            // close statement
-            if (prepStmt != null)
-                try {
-                    prepStmt.close();
-                    
-                } catch (SQLException logOrIgnore) {
-                }
-        }
-        
-    }
-    
-    public boolean makeTable(String text) {
-        
-        Connection connection;
-        Statement statement;
-        
-        try {
-            
-            Singleton.getInstance();
-            Singleton.openConnection();
-            
-            connection = Singleton.getConnection();
-            connection.setAutoCommit(true);
-            connection.setReadOnly(false);
-            
-            statement = connection.createStatement();
-            int creationResult = statement.executeUpdate(text);
-            Log.Out("Created Table with Result " + creationResult, 1, true);
-            
-            statement.close();
-            Singleton.closeConnection();
-            return true;
-            
-        } catch (SQLException | DataAccessException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            Singleton.closeConnection();
-            return false;
-        }
-    }
-    
-    public void processCollection(Collection<ErgoPolyline> collection, String fileName) {
-        
-        String mFileName = fileName.replaceAll(".shp", "");
-        
-        StringBuilder queryStringBuilder = new StringBuilder();
-        StringBuilder schemaStringBuilder = new StringBuilder();
-        StringBuilder valueStringBulder = new StringBuilder();
-        
-        queryStringBuilder.append("CREATE TABLE ");
-        queryStringBuilder.append(mFileName);
-        queryStringBuilder.append(" (ID INT PRIMARY KEY NOT NULL, COORDINATES TEXT");
-        
-        schemaStringBuilder.append("(ID, COORDINATES");
-        valueStringBulder.append("VALUES(?,?");
-        
-        boolean firstPass = true;
-        
-        ErgoPolyline lineZero = collection.iterator().next();
-        for (String key : lineZero.getStringKeys()) {
-            Log.Out(key, 1, true);
-            queryStringBuilder.append(",");
-            queryStringBuilder.append(key);
-            queryStringBuilder.append(" TEXT");
-            
-            schemaStringBuilder.append(" ,");
-            schemaStringBuilder.append(key);
-            
-            valueStringBulder.append(", ?");
+	/**
+	 * Handles SQLite data insertion for an ErgoPolyline object
+	 * @param ID integer id of the object
+	 * @param line Object to be inserted
+	 * @param schema Schema to be used
+	 */
+	public void insertDataToTable(int ID, ErgoPolyline line, String schema) {
+		PreparedStatement prepStmt = null;
 
-        }
-         // TODO Add Type and Vertex List
-        queryStringBuilder.append(")");
-        schemaStringBuilder.append(") ");
-        valueStringBulder.append(");");
-        
-        Log.Out(queryStringBuilder.toString(), 1, true);
-        Log.Out("Creating Table " + mFileName, 1, true);
-        boolean didICreateTable = makeTable(queryStringBuilder.toString());
-        
-        String builderPrefix = "INSERT OR IGNORE INTO " + mFileName + " ";
-        
-        String insertSchema = builderPrefix + schemaStringBuilder.toString() + valueStringBulder.toString();
-        
-        Log.Out("Insert Schema is " + insertSchema, 1, true);
-        
-        try {
-            Singleton.openConnection();
-            Connection connection = Singleton.getConnection();
-            connection.setAutoCommit(true);
-            connection.setReadOnly(false);
-            
-            int id = 0;
-            for (ErgoPolyline line : collection) {
-                id++;
-                insertDataToTable(id, line, insertSchema);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ExportSQL.class.getName()).log(Level.SEVERE, null, ex);
-            Singleton.closeConnection();
-        } finally {
-        	 Singleton.closeConnection();
-        }
-        
-    }
-    
-    public Result<Record> selectQuery(String dbTable, String fieldColumn, String fieldValue) {
-        
-        try {
-            // Open the connection - requires try/catch
-            Singleton.getConnection().setAutoCommit(false);
-            Singleton.getConnection().setReadOnly(true);
-            
-            // Make a new context
-            DSLContext create = DSL.using(Singleton.getConnection(), SQLDialect.SQLITE);
-            
-            Table<?> mTable = DSL.table(dbTable);
-            Field<Object> mColumn = DSL.field(fieldColumn);
-            
-            SelectQuery<Record> query;
-            
-            if(fieldColumn == null || fieldValue == null) {
-                query = create.selectQuery();
-                query.addFrom(mTable);
-            } else {
-                Field<Object> mValue = DSL.field(fieldValue);
-                query = create.selectQuery();
-                query.addFrom(mTable);
-                query.addConditions(mColumn.equal(mValue));
-            }
-            
-            Result<Record> result = query.fetch();
-            Singleton.closeConnection();
-            return result;
-            
-        } catch (SQLException | DataAccessException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            Singleton.closeConnection();
-            System.exit(0);
-        }
-        return null;
-    }
-    
+		try {
+			// use a prepared statement to avoid injections
+
+			Log.Out("Processing " + ID + " for object " + line, 2, false);
+
+
+			prepStmt = Singleton.getConnection().prepareStatement(schema);
+
+			// initialise the column index
+			int countColumn = 1;
+
+			// set ? variables by index order
+			prepStmt.setInt(countColumn, ID);
+
+			// add coordinates
+			countColumn ++;
+			prepStmt.setString(countColumn, line.getVertexListAsString());
+
+			for (String key : line.getStringKeys()) {
+				countColumn++;
+				prepStmt.setString(countColumn, String.valueOf(line.getString(key)));
+
+			}
+
+			// get 0 or 1, depending on success
+			int result = prepStmt.executeUpdate();
+			Log.Out("Update result is " + result, 2, false);
+		} catch (SQLException e) {
+			Log.Exception(e, 1);
+		} finally {
+			// close statement
+			if (prepStmt != null)
+				try {
+					prepStmt.close();
+				} catch (SQLException ex) {
+					Log.Exception(ex, 1);
+				}
+		}
+
+	}
+
+	/**
+	 * Creates a database table
+	 * @param text Name of the table to be created
+	 * @return True if successful
+	 */
+	public boolean makeTable(String text) {
+		try {
+			// initialise connection
+			Singleton.getInstance();
+			Singleton.openConnection();
+
+			Connection connection = Singleton.getConnection();
+			if (connection == null)
+				return false;
+			
+			connection.setAutoCommit(true);
+			connection.setReadOnly(false);
+
+			Statement statement = connection.createStatement();
+			int creationResult = statement.executeUpdate(text);
+			Log.Out("Created Table with Result " + creationResult, 1, true);
+
+			statement.close();
+			
+			return true;
+		} catch (SQLException | DataAccessException e) {
+			Log.Exception(e, 0);
+			return false;
+		} finally {
+			Singleton.closeConnection();
+		}
+	}
+
+	/**
+	 * Handles database entry for a Collection of ErgoPolyline
+	 * @param collection ErgoPolyline Collection
+	 * @param fileName Location of tha database
+	 */
+	public void processCollection(Collection<ErgoPolyline> collection, String fileName) {
+
+		String mFileName = fileName.replaceAll(".shp", "");
+
+		StringBuilder queryStringBuilder = new StringBuilder();
+		StringBuilder schemaStringBuilder = new StringBuilder();
+		StringBuilder valueStringBulder = new StringBuilder();
+
+		queryStringBuilder.append("CREATE TABLE ");
+		queryStringBuilder.append(mFileName);
+		queryStringBuilder.append(" (ID INT PRIMARY KEY NOT NULL, COORDINATES TEXT");
+
+		schemaStringBuilder.append("(ID, COORDINATES");
+		valueStringBulder.append("VALUES(?,?");
+
+		ErgoPolyline lineZero = collection.iterator().next();
+		for (String key : lineZero.getStringKeys()) {
+			Log.Out(key, 1, true);
+			if (key.equalsIgnoreCase("ID")) {
+				key = "ID_1";
+			}
+			if (key.equalsIgnoreCase("COORDINATES")) {
+				key = "COORDINATES_1";
+			}
+			
+			queryStringBuilder.append(",");
+			queryStringBuilder.append(key);
+			queryStringBuilder.append(" TEXT");
+
+			schemaStringBuilder.append(" ,");
+			schemaStringBuilder.append(key);
+
+			valueStringBulder.append(", ?");
+
+		}
+		// TODO Add Type and Vertex List
+		queryStringBuilder.append(")");
+		schemaStringBuilder.append(") ");
+		valueStringBulder.append(");");
+
+		Log.Out(queryStringBuilder.toString(), 1, true);
+		Log.Out("Creating Table " + mFileName, 1, true);
+		boolean didICreateTable = makeTable(queryStringBuilder.toString());
+
+		if (didICreateTable) {
+			Log.Out("Table did not exist so it had to be created", 0, false);
+		}
+
+		String builderPrefix = "INSERT OR IGNORE INTO " + mFileName + " ";
+
+		String insertSchema = builderPrefix + schemaStringBuilder.toString() + valueStringBulder.toString();
+
+		Log.Out("Insert Schema is " + insertSchema, 1, true);
+
+		try {
+			Singleton.openConnection();
+			Connection connection = Singleton.getConnection();
+			connection.setAutoCommit(true);
+			connection.setReadOnly(false);
+
+			int id = 0;
+			for (ErgoPolyline line : collection) {
+				id++;
+				insertDataToTable(id, line, insertSchema);
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(ExportSQL.class.getName()).log(Level.SEVERE, null, ex);
+			Singleton.closeConnection();
+		} finally {
+			Singleton.closeConnection();
+		}
+
+	}
+
+	public Result<Record> selectQuery(String dbTable, String fieldColumn, String fieldValue) {
+
+		try {
+			// Open the connection - requires try/catch
+			Singleton.getConnection().setAutoCommit(false);
+			Singleton.getConnection().setReadOnly(true);
+
+			// Make a new context
+			DSLContext create = DSL.using(Singleton.getConnection(), SQLDialect.SQLITE);
+
+			Table<?> mTable = DSL.table(dbTable);
+			Field<Object> mColumn = DSL.field(fieldColumn);
+
+			SelectQuery<Record> query;
+
+			if(fieldColumn == null || fieldValue == null) {
+				query = create.selectQuery();
+				query.addFrom(mTable);
+			} else {
+				Field<Object> mValue = DSL.field(fieldValue);
+				query = create.selectQuery();
+				query.addFrom(mTable);
+				query.addConditions(mColumn.equal(mValue));
+			}
+
+			Result<Record> result = query.fetch();
+			Singleton.closeConnection();
+			return result;
+
+		} catch (SQLException | DataAccessException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			Singleton.closeConnection();
+			System.exit(0);
+		}
+		return null;
+	}
+
 }
